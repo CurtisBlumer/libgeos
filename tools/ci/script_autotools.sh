dbaf95daa876c7072b4e26a75c106ce507cd453a
Curtis Blumer <curtis.blumer@gmail.com>
2016-06-09 06:40:10 -0500

    Trying to make coveralls.io integration work.

--- tools/ci/script_autotools.sh
+++ tools/ci/script_autotools.sh
@@ -14,6 +14,6 @@ source ${TRAVIS_BUILD_DIR}/tools/ci/common.sh
 cd ${TRAVIS_BUILD_DIR}
 ./autogen.sh
 cd -
-${TRAVIS_BUILD_DIR}/configure --enable-coverage
+${TRAVIS_BUILD_DIR}/configure
 run_make
 run_test
